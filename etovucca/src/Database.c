4ae409c085f8c2c2afc485c18da52070d5eca157
yangm12 <stayyang@yahoo.com>
2024-11-11 17:22:36 -0500

    Finnaly got SQL injection workinggit -m commit git -m commit

--- etovucca/src/Database.c
+++ etovucca/src/Database.c
@@ -25,18 +25,24 @@ _id_t storeElection(sqlite3 *db, Date deadline) {
 
 _id_t storeOffice(sqlite3 *db, _id_t election, char *name) {
    _id_t id = 0;
-   sqlite3_stmt *stmt;
-   const char *sql = "INSERT INTO Office(name, election) VALUES (?, ?)";
-   sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-   sqlite3_bind_text(stmt, 1, name, (int)strnlen(name, MAX_NAME_LEN),
-                     SQLITE_STATIC);
-   sqlite3_bind_int(stmt, 2, election);
-   sqlite3_step(stmt);
-   if (sqlite3_finalize(stmt) == SQLITE_OK) {
+   char *errMsg = 0;
+   char query[256];
+
+   sprintf(query, \
+   "INSERT INTO Office(name, election) VALUES ('%s', %d)", \
+   name, election);
+   
+   if(sqlite3_exec(db, query, NULL, NULL, &errMsg) == SQLITE_OK) {
       id = (_id_t)sqlite3_last_insert_rowid(db);
+   } else {
+      if(errMsg) {
+         fprintf(stderr, "SQL error: %s\n", errMsg);
+         sqlite3_free(errMsg);
+      }
    }
    return id;
 }
+
 
 _id_t storeCandidate(sqlite3 *db, _id_t office, char *name) {
    _id_t id = 0;
